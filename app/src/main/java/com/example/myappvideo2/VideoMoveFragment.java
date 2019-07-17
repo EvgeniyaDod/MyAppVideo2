5645614db5fe1cf1124133a59381ea2b69021c4e
EvgeniyaDod <dodonova.evg@yandex.ru>
2019-07-17 19:51:29 +0400

    commit

--- app/src/main/java/com/example/myappvideo2/VideoMoveFragment.java
+++ app/src/main/java/com/example/myappvideo2/VideoMoveFragment.java
@@ -3,7 +3,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
